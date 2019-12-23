407a4dfb9390ebfe386a8a8d19ee0f3b0b42647b
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2019-12-23 15:01:43 -0300

    set state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Greet } from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
   render(){
   return (
     <div className="App">
+      <Counter />
       < Greet name = "Adel" heroName = "Darkonnen" >
       <p> This is children props </p>
       </Greet>
