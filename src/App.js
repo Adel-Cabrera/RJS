cf297bc1e4cf948ae5162fc3d99a32a9c707b8d8
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2019-12-23 16:17:20 -0300

    Event handling in function & Class

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 class App extends Component {
   render(){
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
       <Counter />
       < Greet name = "Adel" heroName = "Darkonnen" >
       <p> This is children props </p>
